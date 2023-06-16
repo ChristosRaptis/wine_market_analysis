e542b651dd348c6c994f86b4308ea160a539cdf5
Chris Raptis <chris@Chriss-MacBook-Pro.local>
2023-06-16 12:03:45 +0200

    ready for deployment

---
+++ home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from PIL import Image
+
+image = Image.open('assets/ratings-en1.png')
+
+st.markdown('<div style="font-size:45px; font-weight:bold;text-align:center">\
+            Report on wine market analysis based on data gathered from users of the Vivino app</div>', unsafe_allow_html=True)
+
+
+left_col, center_col, right_col = st.columns([0.1, 0.8, 0.1])
+
+left_col.write("")
+
+center_col.image(image, width=500)
+
+right_col.write("")
--- queries/top_three_grapes.sql
+++ queries/top_three_grapes.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT g.name
+SELECT DISTINCT g.name, m.wines_count
 FROM most_used_grapes_per_country AS m 
 INNER JOIN grapes AS g 
 ON m.grape_id = g.id
